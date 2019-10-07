22d66b90bb33e32a9dba6cdd8381251c5dd687c9
PiusAnggoro <piusanggoro@gmail.com>
2019-10-07 14:16:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter responsive ui demos 
+
+flutter responsive UI demos
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/OrientationExample.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OrientationExample extends StatefulWidget {
+  @override
+  _OrientationExampleState createState() => _OrientationExampleState();
+}
+
+class _OrientationExampleState extends State<OrientationExample> {
+  @override
+  Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(),
+      body: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          return Center(
+            child: Text(orientation.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
